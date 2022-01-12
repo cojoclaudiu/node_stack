b7d0118540c3adabf601380f62dd49575de6bbdd
Claudiu Cojocaru <62800983+cojoclaudiu@users.noreply.github.com>
2022-01-12 14:49:55 +0000

    Query database, seed data

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,43 @@
 import express from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+import { nanoid } from 'nanoid';
 
 dotenv.config();
 
 const PORT = Number(process.env.PORT ?? 3080);
+const db = new PrismaClient({ log: ['error', 'info', 'query', 'warn'] });
+const genId = () => nanoid(16);
+
+const seedDatabase = async () => {
+  if ((await db.post.count()) === 0) {
+    await db.post.createMany({
+      data: [
+        {
+          id: genId(),
+          slug: 'node-stack',
+          title: 'Ultimate Node Stack 2022',
+          publishedAt: new Date(),
+        },
+        {
+          id: genId(),
+          slug: 'draft',
+          title: 'Draft post',
+        },
+      ],
+    });
+  }
+};
+seedDatabase();
 
 const app = express();
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.json({
-    test: 'hello claudiu',
-  });
+app.get('/', async (req, res) => {
+  const posts = await db.post.findMany();
+
+  res.json(posts);
 });
 
 app.listen(PORT, '0.0.0.0', () => {
