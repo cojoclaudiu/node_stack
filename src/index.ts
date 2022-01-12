5e005334d6bc0429fc537c024322cb1ab25acc39
Claudiu Cojocaru <62800983+cojoclaudiu@users.noreply.github.com>
2022-01-12 11:58:41 +0000

    Add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log('hello world');
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+// dotenv.config();
+
+const PORT = Number(process.env.PORT ?? 3080);
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.json({
+    test: 'hello world',
+  });
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`server running at http://localhost:${PORT}`);
+});
